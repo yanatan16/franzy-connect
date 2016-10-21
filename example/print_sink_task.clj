f48a1e1f4e4714461e29d4b9c67b8d147cbf0d22
Jon Eisen <jon@joneisen.me>
2016-10-20 23:26:58 -0600

    Version 0.1.0

--- example/print_sink_task.clj
+++
@@ -1,8 +0,0 @@
-(ns print-sink-task
-  (:require [franzy.connect.sink :as sink]))
-
-(sink/make-task
- franzy.connect.example.PrintSink
- {:start #(do (println :print-task :started %) %)
-  :stop #(println :print-task :stopped %)
-  :put-1 #(println :print-sink :put-1 (:print.prefix %1) %2)})
--- project.clj
+++ project.clj
@@ -1,14 +1,16 @@
-(defproject org.clojars.yanatan16/franzy-connect "0.1.0-SNAPSHOT"
+(defproject org.clojars.yanatan16/franzy-connect "0.1.0"
   :description "Clojure interface for making Kafka Connectors"
   :url "https://github.com/yanatan16/franzy-connect"
   :license {:name "MIT"
             :url "https://github.com/yanatan16/franzy-connect/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.kafka/connect-api "0.10.0.1"]
-                 [ymilky/franzy-common "0.0.1"]]
+                 [org.apache.kafka/connect-api "0.10.0.1"]]
 
-  :profiles {:uberjar {:source-paths ["example"]
+  :profiles {:uberjar {:source-paths ["examples"]
                        :aot :all
                        :uberjar-name "franzy-connect-test-standalone.jar"}
              :dev {:source-paths ["example"]
-                   :aot [print-sink]}})
+                   :aot [print-sink]}}
+
+  :plugins [[lein-codox "0.9.0"]]
+  :deploy-repositories [["releases" :clojars]])
